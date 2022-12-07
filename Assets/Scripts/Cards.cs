b651996a77aa74e10e650cbcf36713575ccc260f
Mylan <developerspanly@icloud.com>
2022-12-07 23:07:50 +0100

    Cards Code Optimisation + New Feature

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -6,20 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class Cards : MonoBehaviour
 {
-    public GameObject Stive;
-    public GameObject PrLeton;
-    public GameObject EthanSummer;
-    public GameObject Sonic;
-    public GameObject Claude;
-    public GameObject TomKoon;
-    public GameObject Pika;
-    public GameObject Zailda;
-    public GameObject Mario;
-    public GameObject Remi;
-
     public GameObject BackgroundCards;
     public GameObject Canvas;
-
     public GameObject Stive_OpenCard;
     public GameObject PrLeton_OpenCard;
     public GameObject EthanSummer_OpenCard;
@@ -30,9 +18,42 @@ public class Cards : MonoBehaviour
     public GameObject Zailda_OpenCard;
     public GameObject Mario_OpenCard;
     public GameObject Remi_OpenCard;
-
-    public int NumberOfCards = 5;
+    public int NumberOfCards = 10;
     public Text NumberOfCardsText;
+    public GameObject locked;
+    public void Start()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            if (PlayerPrefs.GetInt("Cards_" + i) == 1)
+            {
+                GameObject.Find("Cards_" + i).GetComponent<SpriteRenderer>().color = Color.grey;
+                NumberOfCards--;
+                this.transform.position = GameObject.Find("Cards_" + i).GetComponent<Transform>().position;
+                Instantiate(locked, this.transform.position, Quaternion.identity);
+            }
+        }
+        NumberOfCardsText.text = NumberOfCards.ToString();
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                PlayerPrefs.SetInt("Cards_" + i, 1);
+            }
+            SceneManager.LoadScene("Cards");
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                PlayerPrefs.SetInt("Cards_" + i, 0);
+            }
+            SceneManager.LoadScene("Cards");
+        }
+    }
     public void buttonClicked(string _String)
     {
         BackgroundCards.SetActive(true);
@@ -52,7 +73,7 @@ public class Cards : MonoBehaviour
             Mario_OpenCard.SetActive(false);
             Remi_OpenCard.SetActive(false);
         }
-        if(_String == "Stive")
+        if (_String == "Stive")
         {
             Stive_OpenCard.SetActive(true);
         }
@@ -91,91 +112,6 @@ public class Cards : MonoBehaviour
         if (_String == "Remi")
         {
             Remi_OpenCard.SetActive(true);
-        }
-    }
-    public void Start()
-    {
-        if(PlayerPrefs.GetInt("Stive") == 0)
-        {
-            Stive.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("PrLeton") == 0)
-        {
-            PrLeton.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("EthanSummer") == 0)
-        {
-            EthanSummer.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Sonic") == 0)
-        {
-            Sonic.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Claude") == 0)
-        {
-            Claude.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("TomKoon") == 0)
-        {
-            TomKoon.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Pika") == 0)
-        {
-            Pika.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Zailda") == 0)
-        {
-            Zailda.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Mario") == 0)
-        {
-            Mario.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        if (PlayerPrefs.GetInt("Remi") == 0)
-        {
-            Remi.GetComponent<SpriteRenderer>().color = Color.gray;
-            NumberOfCards--;
-        }
-        NumberOfCardsText.text = NumberOfCards.ToString();
-    }
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            PlayerPrefs.SetInt("Stive", 1);
-            PlayerPrefs.SetInt("PrLeton", 1);
-            PlayerPrefs.SetInt("EthanSummer", 1);
-            PlayerPrefs.SetInt("Sonic", 1);
-            PlayerPrefs.SetInt("Claude", 1);
-            PlayerPrefs.SetInt("TomKoon", 1);
-            PlayerPrefs.SetInt("Pika", 1);
-            PlayerPrefs.SetInt("Zailda", 1);
-            PlayerPrefs.SetInt("Mario", 1);
-            PlayerPrefs.SetInt("Remi", 1);
-            SceneManager.LoadScene("Cards");
-        }
-        else if (Input.GetKeyDown(KeyCode.M))
-        {
-            PlayerPrefs.SetInt("Stive", 0);
-            PlayerPrefs.SetInt("PrLeton", 0);
-            PlayerPrefs.SetInt("EthanSummer", 0);
-            PlayerPrefs.SetInt("Sonic", 0);
-            PlayerPrefs.SetInt("Claude", 0);
-            PlayerPrefs.SetInt("TomKoon", 0);
-            PlayerPrefs.SetInt("Pika", 0);
-            PlayerPrefs.SetInt("Zailda", 0);
-            PlayerPrefs.SetInt("Mario", 0);
-            PlayerPrefs.SetInt("Remi", 0);
-            SceneManager.LoadScene("Cards");
         }
     }
 }
